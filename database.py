cb759e06d1e111768178be7fa3358e49402f214a
ShreyansK2000 <gshreyansk4@gmail.com>
2020-02-24 19:43:01 -0800

    history

--- database.py
+++ database.py
@@ -1,14 +1,70 @@
 import pymongo
+from time import time
+import json
+import re
+
+import pdb
 
 def connect_db():
     client = pymongo.MongoClient("localhost", 27017)
     return client.endpoint_test
+    
+def add_history(db, username, native_language, target_language, native_word, target_word):
+    translation = db['translations'].find_one({"native_language" : native_language, "target_language" : target_language, "native_word" : native_word, "target_word" : target_word})
+    if translation is not None:
+        translation_id = translation["_id"]
+    else:
+        translation = {"native_language" : native_language, "target_language" : target_language, "native_word" : native_word, "target_word" : target_word}
+        translation_id = db['translations'].insert_one(translation).inserted_id
+    user = db['users'].find_one({"name" : username})
+    
+    if user is None:
+        return 'ERROR'
+    
+    for history_id in user['history_ids']:
+        if history_id['id'] == translation_id:
+            db['users'].update_one(
+                {"name" : username, "history_ids.id" : translation_id},
+                {"$set" : {"history_ids.$.timestamp" : time()}}
+                )
+            return 'OK'
+    
+    db['users'].update({"name" : username}, {'$push': {"history_ids" :{"timestamp" : time(), "id" : translation_id}}})
+    return 'OK'
 
-def update_history(db, _id, word):
-    db.update_one(
-        { "_id": _id},
-        { "$push": { "history": word }}
-    )
+def remove_history(db, username, native_language, target_language, native_word, target_word):
+    # Remove the translation from the user collection if it exists,
+    translation = db['translations'].find_one({"native_language" : native_language, "target_language" : target_language, "native_word" : native_word, "target_word" : target_word})
+    
+    if translation is None:
+        return 'ERROR'
+    
+    translation_id = translation["_id"]
+    db['users'].update(
+        {"name" : username},
+        {"$pull" : {"history_ids" : {"id" : translation_id}}}
+        )
+    
+    return 'OK'
+
+def get_history(db, username):
+    # Lookup user by username
+    # Sort their history_id value by most recent timestamp first
+    # return this collection
+    user = db['users'].find_one({"name" : username})
+    timestamp_sorted = sorted(user['history_ids'], key=lambda k: k['timestamp'], reverse=True)
+    
+    ret = []
+    for entry in timestamp_sorted:
+        translation = db['translations'].find_one({"_id" : entry['id']})
+        ret.append({
+            "native_language" : translation['native_language'],
+            "target_language" : translation['target_language'],
+            "native" : translation['native_word'],
+            "translated" : translation['target_word']
+            })
+        
+    return {"history": ret}
 
 def create_user(db, name, password):
     if db['users'].find({"name": name}).count() > 0:
--- server.py
+++ server.py
@@ -148,6 +148,43 @@ def authenticate_user():
     else:
         return 'NO_DB'
     
+@app.route('/add_to_history', methods=['GET'])
+def add_to_history():
+    global db
+    name = (request.args.get('name'))
+    native_language = (request.args.get('native_language'))
+    target_language = (request.args.get('target_language'))
+    native_word = (request.args.get('native_word'))
+    target_word = (request.args.get('target_word'))
+    
+    if db is not None:
+        return add_history(db, name, native_language, target_language, native_word, target_word)
+    else:
+        return 'NO_DB'
+    
+@app.route('/remove_from_history', methods=['GET'])
+def remove_from_history():
+    global db
+    name = (request.args.get('name'))
+    native_language = (request.args.get('native_language'))
+    target_language = (request.args.get('target_language'))
+    native_word = (request.args.get('native_word'))
+    target_word = (request.args.get('target_word'))
+    
+    if db is not None:
+        return remove_history(db, name, native_language, target_language, native_word, target_word)
+    else:
+        return 'NO_DB'
+
+@app.route('/get_user_history', methods=['GET'])
+def get_user_history():
+    global db
+    name = (request.args.get('name'))
+    
+    if db is not None:
+        return jsonify(get_history(db, name))
+    else:
+        return 'NO_DB' 
     
 if __name__ == '__main__':
     db = connect_db()
